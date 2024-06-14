f761f0537cbf2886e96001f2a631f9b52b178ba1
Stephen Moore <stephen@delfick.com>
2024-06-14 12:44:36 +1000

    Adding base models for handling the virtual dependency folder

--- extended_mypy_django_plugin/django_analysis/virtual_dependencies/dependency.py
+++ extended_mypy_django_plugin/django_analysis/virtual_dependencies/dependency.py
@@ -1,8 +1,10 @@
 import dataclasses
 from collections.abc import Mapping, Sequence
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING, Generic, cast
 
-from .. import protocols
+from typing_extensions import Self
+
+from .. import project, protocols
 
 
 @dataclasses.dataclass
@@ -14,14 +16,30 @@ class VirtualDependencySummary:
 
 
 @dataclasses.dataclass
-class VirtualDependency:
+class VirtualDependency(Generic[protocols.T_Project]):
     module: protocols.Module
     interface_differentiator: str
     summary: VirtualDependencySummary
     all_related_models: Sequence[protocols.ImportPath]
     concrete_annotations: Mapping[protocols.ImportPath, Sequence[protocols.Model]]
 
+    @classmethod
+    def make(
+        cls,
+        *,
+        discovered_project: protocols.Discovered[protocols.T_Project],
+        module: protocols.Module,
+    ) -> Self:
+        return None  # type: ignore[return-value]
+
 
 if TYPE_CHECKING:
-    _VD: protocols.VirtualDependency = cast(VirtualDependency, None)
+    C_VirtualDependency = VirtualDependency[project.C_Project]
+
+    _VD: protocols.VirtualDependency = cast(VirtualDependency[protocols.P_Project], None)
     _VDS: protocols.VirtualDependencySummary = cast(VirtualDependencySummary, None)
+    _VDM: protocols.P_VirtualDependencyMaker = VirtualDependency[protocols.P_Project].make
+
+    _CVDM: protocols.VirtualDependencyMaker[project.C_Project, C_VirtualDependency] = (
+        VirtualDependency[project.C_Project].make
+    )
