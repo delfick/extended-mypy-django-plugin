c53adbf6569ed5292b4d83f0b64ef5c3a1d98205
Stephen Moore <stephen@delfick.com>
2024-06-15 21:56:51 +1000

    Adding a class representing a report for a virtual dependency
    This object will be used by the mypy plugin to actually do useful
    things

--- extended_mypy_django_plugin/django_analysis/virtual_dependencies/__init__.py
+++ extended_mypy_django_plugin/django_analysis/virtual_dependencies/__init__.py
@@ -1,8 +1,11 @@
 from .dependency import VirtualDependency, VirtualDependencySummary
 from .folder import GeneratedVirtualDependencies, VirtualDependencyGenerator
 from .namer import VirtualDependencyNamer
+from .report import Report, ReportCombiner
 
 __all__ = [
+    "Report",
+    "ReportCombiner",
     "VirtualDependencyNamer",
     "VirtualDependency",
     "VirtualDependencySummary",
