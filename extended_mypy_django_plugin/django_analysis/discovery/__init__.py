d129c2f3eaa93ac6a69d31c1cc38d0bb1215e98e
Stephen Moore <stephen@delfick.com>
2024-06-16 11:08:31 +1000

    Adding more tests/docs for the ImportPathHelper

--- extended_mypy_django_plugin/django_analysis/discovery/__init__.py
+++ extended_mypy_django_plugin/django_analysis/discovery/__init__.py
@@ -1,12 +1,13 @@
 from .concrete_models import ConcreteModelsDiscovery
 from .container import Discovery
-from .import_path import ImportPath
+from .import_path import ImportPath, InvalidImportPath
 from .known_models import DefaultInstalledModulesDiscovery
 from .settings_types import NaiveSettingsTypesDiscovery
 
 __all__ = [
     "Discovery",
     "ImportPath",
+    "InvalidImportPath",
     "DefaultInstalledModulesDiscovery",
     "ConcreteModelsDiscovery",
     "NaiveSettingsTypesDiscovery",
