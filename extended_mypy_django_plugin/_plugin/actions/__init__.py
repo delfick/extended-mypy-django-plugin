fffc020ac7d89d00ceaed47f3d0137e074b922b1
Stephen Moore <stephen@delfick.com>
2024-06-20 12:33:30 +1000

    Move more into plugin.protocols

--- extended_mypy_django_plugin/_plugin/actions/__init__.py
+++ extended_mypy_django_plugin/_plugin/actions/__init__.py
@@ -1,14 +1,13 @@
-from .annotation_resolver import Resolver, ValidContextForAnnotationResolver, make_resolver
+from .annotation_resolver import AnnotationResolver, make_resolver
 from .sem_analyze import SemAnalyzing, TypeAnalyzer
 from .type_checker import SharedCheckTypeGuardsLogic, SharedModifyReturnTypeLogic, TypeChecking
 
 __all__ = [
     "SemAnalyzing",
     "TypeAnalyzer",
-    "Resolver",
-    "make_resolver",
-    "ValidContextForAnnotationResolver",
     "TypeChecking",
     "SharedCheckTypeGuardsLogic",
     "SharedModifyReturnTypeLogic",
+    "AnnotationResolver",
+    "make_resolver",
 ]
