78ad7bf1ce0bfe7337d896ac2d442a78e6919786
Stephen Moore <stephen@delfick.com>
2024-06-24 13:46:03 +1000

    Some comments for creating and installing virtual deps/reports

--- extended_mypy_django_plugin/django_analysis/virtual_dependencies/folder.py
+++ extended_mypy_django_plugin/django_analysis/virtual_dependencies/folder.py
@@ -39,6 +39,8 @@ class VirtualDependencyInstaller(Generic[protocols.T_VirtualDependency, protocol
         virtual_namespace: protocols.ImportPath,
         report_factory: protocols.ReportFactory[protocols.T_VirtualDependency, protocols.T_Report],
     ) -> protocols.CombinedReport[protocols.T_Report]:
+        # Determine what would be written to represent the virtual dependencies
+        # And gather each report so we can later combine them into the final report
         reports: list[protocols.T_Report] = []
         written_dependencies: list[protocols.RenderedVirtualDependency[protocols.T_Report]] = []
         for rendered in report_factory.deploy_scribes(self.virtual_dependencies):
@@ -51,17 +53,22 @@ class VirtualDependencyInstaller(Generic[protocols.T_VirtualDependency, protocol
             written_dependencies.append(rendered)
             reports.append(rendered.report)
 
+        # Install our on disk representation into the destination
         report_factory.report_installer.install_reports(
             scratch_root=scratch_root,
             destination=destination,
             virtual_namespace=virtual_namespace,
         )
+
+        # Figure out a string representing the state of everything
         version = report_factory.determine_version(
             destination=destination,
             virtual_namespace=virtual_namespace,
             project_version=self.project_version,
             written_dependencies=written_dependencies,
         )
+
+        # Create our final report
         return report_factory.report_combiner_maker(reports=reports).combine(
             version=version,
             write_empty_virtual_dep=functools.partial(
@@ -78,6 +85,14 @@ class VirtualDependencyInstaller(Generic[protocols.T_VirtualDependency, protocol
         destination: pathlib.Path,
         report_factory: protocols.ReportFactory[protocols.T_VirtualDependency, protocols.T_Report],
     ) -> protocols.ImportPath | None:
+        """
+        This method gets used at mypy time when we come across modules that we believe contains Django
+        models that aren't included in settings.INSTALLED_APPS
+
+        This is so when they are put into settings.INSTALLED_APPS, they don't also need to be modified
+        to have a virtual dependency we can use to tell mypy to re-analyze the file on relevant changes
+        in other parts of the codebase.
+        """
         virtual_import_path = self.virtual_dependency_namer(module_import_path)
         location = destination / f'{virtual_import_path.replace(".", os.sep)}.py'
         if location.exists():
