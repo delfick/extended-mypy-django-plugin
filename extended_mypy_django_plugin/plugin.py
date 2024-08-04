86a83b5aab38387ba28de01c87af2a73ded803ca
Stephen Moore <stephen@delfick.com>
2024-08-05 09:58:57 +1000

    Change the hook machinery to be more efficient
    The existing way the hook machinery works means lots of class
    instantiation whenever the hooks are run, which appears to be a bit of a
    performance drag given enough use of the hooks.
    
    This change makes it so that we instead only instantiate the hook once
    and avoid wrapping the hooks as much as we can

--- extended_mypy_django_plugin/plugin.py
+++ extended_mypy_django_plugin/plugin.py
@@ -1,13 +1,11 @@
-from ._plugin import protocols
+from ._plugin import hook, protocols
 from ._plugin.config import ExtraOptions
 from ._plugin.entry import PluginProvider
-from ._plugin.hook import Hook, hook
 from ._plugin.plugin import ExtendedMypyStubs
 from ._plugin.virtual_dependencies import VirtualDependencyHandler, VirtualDependencyHandlerBase
 
 __all__ = [
     "hook",
-    "Hook",
     "protocols",
     "ExtraOptions",
     "PluginProvider",
